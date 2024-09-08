facf7564a35b38e5ef0f53614a1d0fb4bfb2db34
Ibrahim Dursun <ibrahim@dursun.cc>
2024-09-08 22:32:05 +0100

    fix: tsserver name

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,7 +12,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
           require('mason-lspconfig').setup({
-            ensure_installed = { "lua_ls", "rust_analyzer", "zls", "gopls", "yamlls", "emmet_ls", "tailwindcss", "tsserver" }
+            ensure_installed = { "lua_ls", "rust_analyzer", "zls", "gopls", "yamlls", "emmet_ls", "tailwindcss", "ts_ls" }
           })
         end,
       },
