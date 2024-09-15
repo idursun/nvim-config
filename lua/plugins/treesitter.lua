34c4ba22478094ae85c939218813e6083a6dd67a
Ibrahim Dursun <ibrahim@dursun.cc>
2024-09-15 21:17:31 +0100

    refactor(treesitter): remove custom parser directory

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,14 +3,12 @@ return {
   build = ":TSUpdate",
   enabled = true,
   config = function()
-    vim.opt.runtimepath:prepend("/Users/idursun/.config/nvim/parsers")
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
       highlight = { enable = true },
       indent = { enable = true },
       sync_install = false,
-      parser_install_dir = "/Users/idursun/.config/nvim/parsers",
       auto_install = false,
       ensure_installed = {
         "c",
