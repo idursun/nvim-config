474b7f1dc97527114ca0aeade7b284ea8af9d5c7
Ibrahim Dursun <ibrahim@dursun.cc>
2024-09-17 00:09:56 +0100

    feat: make nvim-tree toggling silent

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -11,6 +11,6 @@ return {
     vim.g.loaded_netrwPlugin = 1
   end,
   keys = {
-    { "<c-n>", ":NvimTreeFindFileToggle<CR>" },
+    { "<c-n>", ":NvimTreeFindFileToggle<CR>", silent = true },
   },
 }
