8a8d6b0b0cf4856b9314b74ce7dfbd2c340ce1c0
Ibrahim Dursun <ibrahim@dursun.cc>
2024-09-17 00:16:23 +0100

    refactor: move key bindings to `keys` block

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -2,6 +2,36 @@ return {
   "ibhagwan/fzf-lua",
   -- optional for icon support
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        require("fzf-lua").files({ cmd = "rg --files" })
+      end,
+      desc = "Find files",
+    },
+    {
+      "<leader>fg",
+      function()
+        require("fzf-lua").grep_project()
+      end,
+      desc = "Grep project",
+    },
+    {
+      "<leader>fr",
+      function()
+        require("fzf-lua").buffers()
+      end,
+      desc = "List buffers",
+    },
+    {
+      "<leader>s",
+      function()
+        require("fzf-lua").lsp_document_symbols()
+      end,
+      desc = "List document symbols",
+    },
+  },
   config = function()
     -- calling `setup` is optional for customization
     require("fzf-lua").setup({
@@ -10,10 +40,5 @@ return {
       },
       file_ignore_patterns = { "^deps/", "^_build/", "^.*/node_modules/" },
     })
-    local actions = require('fzf-lua')
-    vim.keymap.set('n', '<leader>ff', function() actions.files({ cmd = "rg --files" }) end, {})
-    vim.keymap.set('n', '<leader>fg', actions.grep_project, {})
-    vim.keymap.set('n', '<leader>fr', actions.buffers, {})
-    vim.keymap.set('n', '<leader>s', actions.lsp_document_symbols, {})
   end,
 }
