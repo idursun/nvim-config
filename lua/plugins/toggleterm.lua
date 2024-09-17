6376b7a8d2245e5416488e3c3f4cfc4f4315c927
ibrahim dursun <ibrahim@dursun.cc>
2024-09-17 22:45:16 +0100

    feat: add more toggleterm key mappings

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,6 +5,9 @@ return {
   lazy = true,
   keys = {
     { "<leader>tt", ":ToggleTerm<CR>", silent = true },
+    { "<leader>T", ":ToggleTerm size=40 direction=float <CR>", silent = true },
+    { "<leader>tjl", ":TermExec cmd=\"jj log\" size=40 direction=float <CR>", silent = true },
+    { "<leader>tjd", ":TermExec cmd=\"jj diff\" size=40 direction=float <CR>", silent = true },
     { "<leader>ts", ":ToggleTermSendVisualLines<CR>", silent = true, mode = "v" },
   },
 }
