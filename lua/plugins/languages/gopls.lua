b8c1bad0ae1a6a887743706d972c695a9aba2bf5
ibrahim dursun <ibrahim@dursun.cc>
2024-09-23 13:27:04 +0100

    style: remove debug statement

--- lua/plugins/languages/gopls.lua
+++ lua/plugins/languages/gopls.lua
@@ -1,9 +1,8 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
-    print("inside callback")
     local params = vim.lsp.util.make_range_params()
-    params.context = {only = {"source.organizeImports"}}
+    params.context = { only = { "source.organizeImports" } }
     -- buf_request_sync defaults to a 1000ms timeout. Depending on your
     -- machine and codebase, you may want longer. Add an additional
     -- argument after params if you find that you have to write the file
@@ -18,7 +17,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         end
       end
     end
-    vim.lsp.buf.format({async = false})
+    vim.lsp.buf.format({ async = false })
   end
 })
 
